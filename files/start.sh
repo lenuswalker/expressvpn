d66d8ddab828545a24c62285a1f17fe886f19841
Lenus Walker <walker.lenus@gmail.com>
2021-09-04 21:49:51 -0400

    Added routing to be able to use the container as a router.

--- files/start.sh
+++ files/start.sh
@@ -1,13 +1,16 @@
-#!/usr/bin/bash
+#!/bin/bash
 cp /etc/resolv.conf /etc/resolv.conf.bak
 umount /etc/resolv.conf
 cp /etc/resolv.conf.bak /etc/resolv.conf
 rm /etc/resolv.conf.bak
 sed -i 's/DAEMON_ARGS=.*/DAEMON_ARGS=""/' /etc/init.d/expressvpn
+
+/bin/bash /expressvpn/routing.sh
 service expressvpn restart
-expect /expressvpn/activate.sh
-expressvpn preferences set preferred_protocol lightway_udp
-expressvpn preferences set lightway_cipher chacha20
+#expect /expressvpn/activate.sh
+expressvpn activate $ACTIVATION_CODE
+expressvpn preferences set preferred_protocol $PREFERRED_PROTOCOL
+expressvpn preferences set lightway_cipher $LIGHTWAY_CIPHER
 expressvpn preferences set network_lock on
 expressvpn preferences set auto_connect true
 expressvpn connect $SERVER
