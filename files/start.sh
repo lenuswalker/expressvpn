5701b94f1f80c8f0ebe5f183af726779353f43f1
Lenus Walker <walker.lenus@gmail.com>
2021-09-05 17:21:32 -0400

    Update Dockerfile and activate.sh

--- files/start.sh
+++ files/start.sh
@@ -5,15 +5,15 @@ cp /etc/resolv.conf.bak /etc/resolv.conf
 rm /etc/resolv.conf.bak
 sed -i 's/DAEMON_ARGS=.*/DAEMON_ARGS=""/' /etc/init.d/expressvpn
 
-/bin/bash /expressvpn/routing.sh
 service expressvpn restart
-#expect /expressvpn/activate.sh
-expressvpn activate $ACTIVATION_CODE
+expect /expressvpn/activate.sh
 expressvpn preferences set preferred_protocol $PREFERRED_PROTOCOL
 expressvpn preferences set lightway_cipher $LIGHTWAY_CIPHER
 expressvpn preferences set network_lock on
 expressvpn preferences set auto_connect true
 expressvpn connect $SERVER
+
+/bin/bash /expressvpn/routing.sh
 
 touch /var/log/temp.log
 tail -f /var/log/temp.log
