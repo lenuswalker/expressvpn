1e7c30c6e6edf8f892c2c01a276a8befde37940c
Lenus Walker <walker.lenus@gmail.com>
2021-09-05 18:05:38 -0400

    Testing no iptables

--- files/start.sh
+++ files/start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-cp /etc/resolv.conf /etc/resolv.conf.bak
-umount /etc/resolv.conf
-cp /etc/resolv.conf.bak /etc/resolv.conf
-rm /etc/resolv.conf.bak
+
 sed -i 's/DAEMON_ARGS=.*/DAEMON_ARGS=""/' /etc/init.d/expressvpn
 
+/bin/bash /expressvpn/routing.sh
 service expressvpn restart
 expect /expressvpn/activate.sh
 expressvpn preferences set preferred_protocol $PREFERRED_PROTOCOL
@@ -13,9 +11,7 @@ expressvpn preferences set network_lock on
 expressvpn preferences set auto_connect true
 expressvpn connect $SERVER
 
-#/bin/bash /expressvpn/routing.sh
-
 touch /var/log/temp.log
 tail -f /var/log/temp.log
 
-exec "$@"
+#exec "$@"
